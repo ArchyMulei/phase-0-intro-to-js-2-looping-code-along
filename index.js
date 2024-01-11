41a7d9c781dcdc7ce624cd26cf8b145a7de44f8c
Archibald <archimulei023@gmail.com>
2024-01-11 09:26:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, occassion){
+    const messages = []
+    for(let i = 0; i<names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${occassion} gift!`)
+    }
+    return messages
+}
+
+const messages = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(messages);
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+ }
+
+const countdown = countDown(10);
+console.log(countdown);
